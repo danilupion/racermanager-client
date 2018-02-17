9055fdd5776d6a1039f3f4070c25aff4a47bedd2
Victor Manuel Fernandez Lopez <vimafelo@hotmail.es>
2018-02-17 15:24:37 +0100

    Added new validate field password

--- src/app/components/authDialog/register/register.component.ts
+++ src/app/components/authDialog/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
 import { MatSnackBar } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
-import { MyErrorStateMatcher } from '../myErrorStateMatcher';
+import { MyErrorStateMatcher } from '../../../utils/myErrorStateMatcher';
 
 @Component({
   selector: 'rm-auth-register',
@@ -39,6 +39,10 @@ export class RegisterComponent {
   }
 
   validate(): boolean {
-    return !!this.username && !!this.password && !!this.password && this.password === this.repeatedPassword;
+    return !!this.username
+    && !!this.password
+    && !this.emailFormControl.hasError('email')
+    && !!this.password
+    && this.password === this.repeatedPassword;
   }
 }
