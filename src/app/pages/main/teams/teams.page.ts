8b72834d6ffa38b7c492b558fd4e42e7eb441acd
Victor Manuel Fernandez Lopez <vimafelo@hotmail.es>
2018-03-07 18:19:15 +0100

    Start with Teams page Component

--- src/app/pages/main/teams/teams.page.ts
+++ src/app/pages/main/teams/teams.page.ts
@@ -10,17 +10,34 @@ import { SeasonsService } from '../../../services/seasons.service';
 export class TeamsPageComponent implements OnInit, OnDestroy {
   private selectedChampionshipObserverDisposer;
 
+
+  public teams = [];
+  displayedColumns = ['Team', 'Drivers', 'Points', 'Factor'];
+
   constructor(
     public seasonService: SeasonsService,
     private championshipsService: ChampionshipsService,
-  ) { }
+  ) {
+    console.error('TEAMS, ', this.teams);
+   }
+
+  private async update() {
+    try {
+      await this.seasonService.update();
+    } catch (err) {}
+  }
 
   ngOnInit(): void {
     this.selectedChampionshipObserverDisposer = observe(
       this.championshipsService,
       'selected',
-      () => this.seasonService.update(),
+      () => this.update(),
     );
+
+    this.teams = this.seasonService
+              && this.seasonService.selected
+              && this.seasonService.selected.teams;
+    console.error('TEAMS, ', this.teams);
   }
 
   ngOnDestroy(): void {
